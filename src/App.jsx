d359f7e95712f46e0eecbc8c6d435b0fa29bcf36
Orlando-Abril <orlandoabrillujan@gmail.com>
2026-06-29 16:02:56 -0300

    mejor diseño visual

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import GamePage from "./pages/Game/GamePage.jsx";
 import "./App.css";
 
 function App() {
-  return (
-      <div className="main-app">
-        <GamePage />
-      </div>
-  );
+    return (
+        <div className="main-app">
+            <GamePage />
+        </div>
+    );
 }
 
 export default App;
