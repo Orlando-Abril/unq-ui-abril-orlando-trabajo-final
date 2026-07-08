873296a898238f026aaf6f9c5c29904d450c38f0
Orlando-Abril <orlandoabrillujan@gmail.com>
2026-07-08 13:50:36 -0300

    chequeo final

--- src/components/Game/GameHeader.jsx
+++ src/components/Game/GameHeader.jsx
@@ -1,7 +1,6 @@
 const GameHeader = () => {
     return (
         <header className="game-header">
-            <p className="game-kicker">Abril Orlando - Trabajo final</p>
             <h1>Palabras Encadenadas</h1>
         </header>
     );
